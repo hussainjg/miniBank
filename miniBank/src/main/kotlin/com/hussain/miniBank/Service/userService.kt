2fa03fa30787a7b8af0cf7a08be0c5b8c58f754c
hussainjg <79175224+hussainjg@users.noreply.github.com>
2025-04-15 19:59:42 +0300

    Added more guidelines

--- miniBank/src/main/kotlin/com/hussain/miniBank/Service/userService.kt
+++ miniBank/src/main/kotlin/com/hussain/miniBank/Service/userService.kt
@@ -13,16 +13,36 @@ class UsersServices(private val usersRepository: UserRepo) {
 
         val specialRegex = Regex("[^a-zA-Z0-9]")
         val upperRegex = Regex("[A-Z]")
+        val numberRegex = Regex("[0-9]")
 
         if (usersRepository.findByUsername(username) != null) {
             throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Username already exists")
         }
-
-        if (!specialRegex.containsMatchIn(password) || !upperRegex.containsMatchIn(password)) {
-            throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Password must contain at least one special character and upper case letter."
+        if (username.length <= 8) {
+            throw ResponseStatusException(
+                HttpStatus.BAD_REQUEST, "Username must be more than 8 characters."
             )
         }
-
+        if (password.length <= 8) {
+            throw ResponseStatusException(
+                HttpStatus.BAD_REQUEST, "Password must be more than 8 characters."
+            )
+        }
+        if (specialRegex.containsMatchIn(username) || numberRegex.containsMatchIn(username)) {
+            throw ResponseStatusException(
+                HttpStatus.BAD_REQUEST,
+                "Username must not contain special characters or numbers."
+            )
+        }
+        if (!specialRegex.containsMatchIn(password) || !upperRegex.containsMatchIn(password) || !numberRegex.containsMatchIn(
+                password
+            )
+        ) {
+            throw ResponseStatusException(
+                HttpStatus.BAD_REQUEST,
+                "Password must contain at least one special character and upper case letter and numbers."
+            )
+        }
         val newUser = UserEntity(username = username, password = password)
         return usersRepository.save(newUser)
     }
